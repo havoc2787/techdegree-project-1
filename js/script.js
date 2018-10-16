1b187b36c1663bd7eb5ed2405cb26739bfe30dd9
sgutier87 <havoc2787@gmail.com>
2018-10-16 15:14:38 -0400

    Added quotes array with two objects

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
-
+let quotes = [
+  {
+    quote: "It's kinda fun to do the impossible.",
+    source: 'Walt Disney',
+    citation: 'Animated Architecture by by Derek Walker, p. 10',
+    year: 1982
+  },
+  {
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    source: 'Thomas A. Edison',
+    citation: 'The Fabulous Drone by J. L. Elkhorne, pg. 52',
+    year: 1967
+  },
+  {
+    quote: '',
+    source: '',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: '',
+    source: '',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: '',
+    source: '',
+    citation: '',
+    year: ''
+  }
+];
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
@@ -15,3 +46,7 @@
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
+
+
