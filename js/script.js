cd6f0d68764710a169976af2b1707fdad6b3a971
Steven Gutierrez <havoc2787@gmail.com>
2018-10-16 21:11:23 -0400

    Added five more quotes to quote array

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@
 // Create the array of quote objects and name it quotes
 let quotes = [
   {
+    quote: 'Why do we fall? So we can learn to pick ourselves up.',
+    source: 'Thomas Wayne',
+    citation: 'Batman Begins',
+    year: 2005
+  },
+  {
     quote: "It's kinda fun to do the impossible.",
     source: 'Walt Disney',
     citation: 'Animated Architecture by by Derek Walker, p. 10',
@@ -15,22 +21,26 @@ let quotes = [
     year: 1967
   },
   {
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
+    quote: "You miss 100% of the shots you don't take.",
+    source: 'Wayne Gretzky'
   },
   {
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
+    quote: 'When I get sad, I stop being sad and be awesome instead. True Story.',
+    source: 'Barney Stinson',
+    citation: 'How I Met You Mother, 2x01',
+    year: 2006
   },
   {
-    quote: '',
-    source: '',
-    citation: '',
-    year: ''
+    quote: "In three words I can sum up everything I've learned about life: It goes on.",
+    source: 'Robert Frost',
+    citation: "Richard Evans' Quote Book, p. 109",
+    year: 1973
+  },
+  {
+    quote: 'Sometimes I push the door close button on people running towards the elevator. I just need my own elevator sometimes. My sanctuary.',
+    source: 'Kanye West',
+    citation: 'Twitter',
+    year: 2014
   }
 ];
 
